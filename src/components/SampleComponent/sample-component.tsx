eb4051b1338dff6aaba2402b8b7e22c18725825a
aptmac <almacdon@redhat.com>
2024-03-05 15:50:33 -0500

    chore(repo): allow repo to be run without a running local insights-chrome (#2)
    This commit also removes a bunch of leftover code that wasn't going to
    be used. It updates the webpack config so at the moment running this
    locally only requires a running (and modified) chrome-backend-service
    and insights-inventory-frontend. Instructions and link to example
    branches have been added to the readme.

--- src/components/SampleComponent/sample-component.tsx
+++
@@ -1,8 +0,0 @@
-import './sample-component.scss';
-import React from 'react';
-
-const SampleComponent: React.FC<React.PropsWithChildren> = (props) => {
-  return <span className="sample-component"> {props.children} </span>;
-};
-
-export default SampleComponent;
