d6125ced7c9d6e526aba5e164d80d6a3be17d714
Alex Macdonald <almacdon@redhat.com>
2024-03-28 12:21:03 -0400

    feat(card): update the card with info for the initial set of info

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { To } from 'react-router-dom';
+import { JvmInstance } from '../api/interfaces';
 
 export const linkBasename = '/staging/starter';
 export const mergeToBasename = (to: To, basename: string): To => {
@@ -11,4 +12,26 @@ export const mergeToBasename = (to: To, basename: string): To => {
     ...to,
     pathname: `${basename}/${to.pathname}`.replace(`^${basename}//`, '/'),
   };
+};
+
+export const formatInstancesData = (instances: JvmInstance[]) => {
+  instances.forEach((instance) => {
+    // change the unidentified workload type
+    if (instance.workload === 'Unidentified') {
+      instance.workload = 'General Java Application';
+    }
+    // format the date string
+    instance.created = new Date(instance.created).toLocaleString();
+    // add the appName value to a JvmInstance
+    if (!instance['appName']) {
+      instance.appName = instance.details['app.name'];
+    }
+    // format the GC details string
+    const regex = new RegExp('gc::(.*?)::(.*?)$');
+    const match = regex.exec(instance.jvmHeapGcDetails);
+    if (match && match[1] && match[2]) {
+      instance.jvmHeapGcDetails = match[1] + ', ' + match[2];
+    }
+  });
+  return instances;
 };
