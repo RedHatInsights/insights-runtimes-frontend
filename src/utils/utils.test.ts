d3c7d4df3f01f92d35c8e781ff114365285b8dbb
aptmac <almacdon@redhat.com>
2024-04-23 00:24:02 -0400

    fix(utils): handle potentially invalid date time strings (#14)

---
+++ src/utils/utils.test.ts
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom';
+import { formatDateTimeString } from './utils';
+
+describe('Utilities', () => {
+  describe('formatDateTimeString()', () => {
+    it('should format simplified ISO-8061 date time strings', () => {
+      const datestring = '2024-02-21T20:14:29.380Z';
+      expect(formatDateTimeString(datestring)).not.toEqual('Invalid Date');
+    });
+
+    it('should handle ISO-8061 ZonedDateTime strings from Java', () => {
+      const datestring = '2024-02-21T20:14:29.380397Z';
+      expect(formatDateTimeString(datestring)).not.toEqual('Invalid Date');
+    });
+
+    it('should return the passed value as a fallback', () => {
+      const datestring = '2024-02-21T20:14:29.38-05:00Z';
+      expect(formatDateTimeString(datestring)).toBe(datestring);
+    });
+  });
+});
