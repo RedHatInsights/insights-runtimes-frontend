b040673c66a6eff7b3065a381be9f120af110586
Alex Macdonald <almacdon@redhat.com>
2024-06-24 17:02:12 -0400

    fix(MWTELE-271): update the formatting of the date

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { formatDateTimeString } from './utils';
+import { formatDateTimeString, formatExactUTCString } from './utils';
 
 describe('Utilities', () => {
   describe('formatDateTimeString()', () => {
@@ -16,6 +16,13 @@ describe('Utilities', () => {
     it('should return the passed value as a fallback', () => {
       const datestring = '2024-02-21T20:14:29.38-05:00Z';
       expect(formatDateTimeString(datestring)).toBe(datestring);
+    });
+  });
+
+  describe('formatExactUTCString()', () => {
+    it('should return an expected format of DD MMM YYYY HH:MM UTC', () => {
+      const utcstring = 'Fri, 26 Jan 2024 08:08:08 GMT';
+      expect(formatExactUTCString(utcstring)).toBe('26 Jan 2024 08:08 UTC');
     });
   });
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -39,7 +39,20 @@ export const formatInstancesData = (instances: JvmInstance[]) => {
 };
 
 /**
- * Function that tries to convert a date string into a Date object, and returns a locale date string.
+ * To better integrate within the host inventory frontend, we will use the same formatting function they
+ * use to format their date strings. This would be the "exact" function from the frontend-components
+ * DateFormat helper.
+ *
+ * See: https://github.com/RedHatInsights/insights-inventory-frontend/blob/master/src/components/InventoryDetail/FactsInfo.js#L50
+ * See: https://github.com/RedHatInsights/frontend-components/blob/master/packages/components/src/DateFormat/helper.tsx#L41
+ * @param utcString a UTC date string
+ * @returns a datestring formatted to 'exact' as seen in the frontend-components DateFormat component
+ */
+export const formatExactUTCString = (utcstring: string) =>
+  utcstring.split(',')[1].slice(0, -7).trim() + ' UTC';
+
+/**
+ * Function that tries to convert a date string into a Date object, and returns a special UTC date string.
  * The incoming date string can be a timestamp string, or a type of Date Time string.
  *
  * The runtimes-inventory backend stores the "created" time as a Java ZonedDateTime, which follows the
@@ -54,28 +67,34 @@ export const formatInstancesData = (instances: JvmInstance[]) => {
  * See: https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
  *
  * This function attempts to pass the Java ZonedDateTime string into the JavaScript Date constructor,
- * and convert it into a locale string. If the resulting string is 'Invalid Date' then the formatting
+ * and convert it into a UTC string. If the resulting string is 'Invalid Date' then the formatting
  * of the ZonedDateTime failed, and will try to use a regex to grab the simplified Date Time String
  * from the ZonedDateTime. If that fails, it will just return the ZonedDateTime string as written.
  *
  */
-export const formatDateTimeString = (date: string) => {
-  let datestring = new Date(date).toLocaleString();
-  if (datestring === 'Invalid Date') {
+export const formatDateTimeString = (created: string) => {
+  const date = new Date(created);
+  let utcstring = date.toUTCString();
+  let datestring = '';
+  if (utcstring !== 'Invalid Date') {
+    datestring = formatExactUTCString(utcstring);
+  } else {
     const regex = new RegExp(
       '^([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3})[0-9]{3}(Z|[+-][0-9]{2}:[0-9]{2})$'
     );
-    const match = regex.exec(date);
+    const match = regex.exec(created);
     if (match && match[1] && match[2]) {
-      datestring = new Date(
+      utcstring = new Date(
         match[1].valueOf().concat(match[2].valueOf())
-      ).toLocaleString();
-      if (datestring === 'Invalid Date') {
+      ).toUTCString();
+      if (utcstring === 'Invalid Date') {
         // if the regex capture works but still results in an invalid date, just return the ISO-8061
-        datestring = date;
+        datestring = created;
+      } else {
+        datestring = formatExactUTCString(datestring);
       }
     } else {
-      datestring = date;
+      datestring = created;
     }
   }
   return datestring;
