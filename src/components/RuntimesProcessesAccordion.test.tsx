d7279f2dd0fb2004a071f6b06036aca17feb30fb
aptmac <almacdon@redhat.com>
2024-07-17 10:16:32 -0400

    fix(MWTELE-271): Insights UI display fails (#18)
    * fix(MWTELE-271): update the formatting of the date
    
    * fix(MWTELE-271): generate an instance.title if workload and app.name are both empty
    
    * fix(MWTELE-240): remove details from jvm instance

--- src/components/RuntimesProcessesAccordion.test.tsx
+++ src/components/RuntimesProcessesAccordion.test.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import RuntimesProcessesAccordion from './RuntimesProcessesAccordion';
 import '@testing-library/jest-dom';
-import { fooInstance, mockInstance } from '../utils/test-utils';
+import {
+  fooInstance,
+  getCopyOfInstance,
+  mockInstance,
+} from '../utils/test-utils';
 import { formatInstancesData } from '../utils/utils';
 
 describe('Runtimes Processes Accordion', () => {
   it('should display instance information', () => {
     const { container } = render(
       <RuntimesProcessesAccordion
-        instances={formatInstancesData([mockInstance])}
+        instances={formatInstancesData([getCopyOfInstance(mockInstance)])}
       />
     );
     expect(
       container.querySelector('.pf-v5-c-accordion__toggle-text')?.innerHTML
-    ).toEqual(mockInstance.title);
+    ).toEqual('mockWorkload');
   });
 
   it('should be expanded if there is only one response', () => {
     const { container } = render(
       <RuntimesProcessesAccordion
-        instances={formatInstancesData([mockInstance])}
+        instances={formatInstancesData([getCopyOfInstance(mockInstance)])}
       />
     );
     expect(
@@ -33,7 +37,10 @@ describe('Runtimes Processes Accordion', () => {
   it('should not be expanded if there is more than one', () => {
     const { container } = render(
       <RuntimesProcessesAccordion
-        instances={formatInstancesData([fooInstance, mockInstance])}
+        instances={formatInstancesData([
+          getCopyOfInstance(fooInstance),
+          getCopyOfInstance(mockInstance),
+        ])}
       />
     );
     expect(
@@ -46,7 +53,7 @@ describe('Runtimes Processes Accordion', () => {
   it('should toggle row expansion when clicked', () => {
     const { container } = render(
       <RuntimesProcessesAccordion
-        instances={formatInstancesData([mockInstance])}
+        instances={formatInstancesData([getCopyOfInstance(mockInstance)])}
       />
     );
     const fooButton = container.querySelector('#instance-0-toggle');
@@ -69,5 +76,19 @@ describe('Runtimes Processes Accordion', () => {
         .querySelector('#instance-0-toggle')
         ?.className.includes('pf-m-expanded')
     ).toBe(true);
+  });
+
+  // MWTELE-271
+  it('should generate a title if both workload and app.name is empty', () => {
+    const modifiedInstance = getCopyOfInstance(mockInstance);
+    modifiedInstance.workload = '';
+    const { container } = render(
+      <RuntimesProcessesAccordion
+        instances={formatInstancesData([modifiedInstance])}
+      />
+    );
+    expect(
+      container.querySelector('.pf-v5-c-accordion__toggle-text')?.innerHTML
+    ).toEqual('Unknown workload');
   });
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -17,14 +17,28 @@ export const mergeToBasename = (to: To, basename: string): To => {
 export const formatInstancesData = (instances: JvmInstance[]) => {
   instances.forEach((instance) => {
     instance.title = instance.workload;
-    // add the appName value to a JvmInstance
+    // if there is no appName, use the workload as it's accordion title
     if (!instance['appName']) {
-      instance.appName = instance.details['app.name'];
+      instance.title = instance.workload;
     }
     // change the unidentified workload type, and set the title to the application name
     if (instance.workload === 'Unidentified') {
-      instance.workload = `General Java Application`;
-      instance.title = instance.appName;
+      instance.workload = 'General Java Application';
+      if (instance.appName) {
+        // if it's an eap instance, use the app name as it's title
+        instance.title = instance.appName;
+      } else {
+        // if it's a jvm instance, use the workload as it's title
+        instance.title = instance.workload;
+      }
+    } else if (instance.workload === '') {
+      // MWTELE-271: the UI should accommodate redacted workload & app.name
+      instance.workload = 'Unknown workload';
+      if (instance.appName) {
+        instance.title = instance.appName;
+      } else {
+        instance.title = instance.workload;
+      }
     }
     // format the date string
     instance.created = formatDateTimeString(instance.created);
@@ -39,7 +53,20 @@ export const formatInstancesData = (instances: JvmInstance[]) => {
 };
 
 /**
- * Function that tries to convert a date string into a Date object, and returns a locale date string.
+ * To better integrate within the host inventory frontend, we will use the same formatting function they
+ * use to format their date strings. This would be the "exact" function from the frontend-components
+ * DateFormat helper.
+ *
+ * See: https://github.com/RedHatInsights/insights-inventory-frontend/blob/master/src/components/InventoryDetail/FactsInfo.js#L50
+ * See: https://github.com/RedHatInsights/frontend-components/blob/master/packages/components/src/DateFormat/helper.tsx#L41
+ * @param utcString a UTC date string
+ * @returns a datestring formatted to 'exact' as seen in the frontend-components DateFormat component
+ */
+export const formatExactUTCString = (utcstring: string) =>
+  utcstring.split(',')[1].slice(0, -7).trim() + ' UTC';
+
+/**
+ * Function that tries to convert a date string into a Date object, and returns a special UTC date string.
  * The incoming date string can be a timestamp string, or a type of Date Time string.
  *
  * The runtimes-inventory backend stores the "created" time as a Java ZonedDateTime, which follows the
@@ -54,28 +81,34 @@ export const formatInstancesData = (instances: JvmInstance[]) => {
  * See: https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
  *
  * This function attempts to pass the Java ZonedDateTime string into the JavaScript Date constructor,
- * and convert it into a locale string. If the resulting string is 'Invalid Date' then the formatting
+ * and convert it into a UTC string. If the resulting string is 'Invalid Date' then the formatting
  * of the ZonedDateTime failed, and will try to use a regex to grab the simplified Date Time String
  * from the ZonedDateTime. If that fails, it will just return the ZonedDateTime string as written.
  *
  */
-export const formatDateTimeString = (date: string) => {
-  let datestring = new Date(date).toLocaleString();
-  if (datestring === 'Invalid Date') {
+export const formatDateTimeString = (created: string) => {
+  const date = new Date(created);
+  let utcstring = date.toUTCString();
+  let datestring = '';
+  if (utcstring !== 'Invalid Date') {
+    datestring = formatExactUTCString(utcstring);
+  } else {
     const regex = new RegExp(
       '^([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3})[0-9]{3}(Z|[+-][0-9]{2}:[0-9]{2})$'
     );
-    const match = regex.exec(date);
+    const match = regex.exec(created);
     if (match && match[1] && match[2]) {
-      datestring = new Date(
+      utcstring = new Date(
         match[1].valueOf().concat(match[2].valueOf())
-      ).toLocaleString();
-      if (datestring === 'Invalid Date') {
+      ).toUTCString();
+      if (utcstring === 'Invalid Date') {
         // if the regex capture works but still results in an invalid date, just return the ISO-8061
-        datestring = date;
+        datestring = created;
+      } else {
+        datestring = formatExactUTCString(datestring);
       }
     } else {
-      datestring = date;
+      datestring = created;
     }
   }
   return datestring;
